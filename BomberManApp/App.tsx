d8303dc9d4decdf71710fb3c2d994f5039c01644
FormalHat <160233763+thz2501@users.noreply.github.com>
2024-09-06 11:49:47 -0300

    abc
    123

--- BomberManApp/App.tsx
+++ BomberManApp/App.tsx
@@ -2,13 +2,38 @@ import React from "react";
 import {View,Text,StyleSheet} from 'react-native'
    
 
-export default function app (){return(
-  <View>
-    <View>
-
+export default function app (){
+  return(
+  <View style={styles.container}>
+    <View style={styles.box1}>
+      <Text>Box 1</Text>
     </View>
-    <View>
-
+    <View style={styles.box2}>
+      <Text>Box 2</Text>
     </View>
   </View>
-)}
+);
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor: '#f0f0f0'
+  },
+  box1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'lightpurple',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  box2: {
+    width: 100, 
+    height: 100,
+    backgroundColor: 'salmon',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
