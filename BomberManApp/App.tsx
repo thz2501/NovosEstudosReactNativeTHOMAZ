a0ccbeb280c54a14fa9e02729197201a74b3b009
FormalHat <160233763+thz2501@users.noreply.github.com>
2024-09-06 08:41:19 -0300

    abc
    432

---
+++ BomberManApp/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View,Text,StyleSheet} from 'react-native'
+   
+
+export default function app (){return(
+  <View>
+    <View>
+
+    </View>
+    <View>
+
+    </View>
+  </View>
+)}
